bfc5cbcd1f7148723d3c613a7e9405ddbff5ca76
Syazwan Fitri <syazfitry69@gmail.com>
2021-11-13 12:37:48 +0800

    Change the price when slider is moving

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const slider = document.querySelector('.slider');
-const pageViewValue = document.querySelector('.pageviews-value')
+const pageViewValue = document.querySelector('.pageviews-value');
+const priceValue = document.querySelector('.price-value');
+let num = 1;
 
 pageViewValue.textContent = slider.value;
+priceValue.textContent = parseInt(slider.value, 10) + num; 
 
 slider.oninput = function () {
     pageViewValue.textContent = this.value;
+    priceValue.textContent = parseInt(slider.value, 10) + num;
 }
