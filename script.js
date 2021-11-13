b592a7f256bfe68e500da60341bbdea8b4ec09a6
Syazwan Fitri <syazfitry69@gmail.com>
2021-11-13 12:16:17 +0800

    Remove comment

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const slider = document.querySelector('.slider');
 const pageViewValue = document.querySelector('.pageviews-value')
 
-// pageViewValue.textContent = slider.value;
+pageViewValue.textContent = slider.value;
 
 slider.oninput = function () {
     pageViewValue.textContent = this.value;
