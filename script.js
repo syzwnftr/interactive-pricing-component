786e976e2e234a05830fd12dfe9e79bceb338375
Syazwan Fitri <syazfitry69@gmail.com>
2021-11-13 12:48:24 +0800

    Added function to show 2 decimal places for price

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const slider = document.querySelector('.slider');
 const pageViewValue = document.querySelector('.pageviews-value');
 const priceValue = document.querySelector('.price-value');
+
 let num = 1;
 
 pageViewValue.textContent = slider.value;
-priceValue.textContent = parseInt(slider.value, 10) + num; 
+priceValue.textContent = Math.round((parseInt(slider.value, 10) + num)).toFixed(2); 
 
 slider.oninput = function () {
     pageViewValue.textContent = this.value;
-    priceValue.textContent = parseInt(slider.value, 10) + num;
+    priceValue.textContent = Math.round((parseInt(slider.value, 10) + num)).toFixed(2);
 }
+
