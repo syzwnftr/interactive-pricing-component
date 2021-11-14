fe059f688715122bc74cdbcccc09d2f9fd44a8e6
Syazwan Fitri <syazfitry69@gmail.com>
2021-11-14 16:07:16 +0800

    Added function to change price when slider is moved

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ const slider = document.querySelector('.slider');
 const pageViewValue = document.querySelector('.pageviews-value');
 const priceValue = document.querySelector('.price-value');
 
-let num = 1;
 
 pageViewValue.textContent = slider.value;
-priceValue.textContent = Math.round((parseInt(slider.value, 10) + num)).toFixed(2); 
+priceValue.textContent = (parseInt(slider.value) / 2).toFixed(2);
+
+
 
 slider.oninput = function () {
     pageViewValue.textContent = this.value;
-    priceValue.textContent = Math.round((parseInt(slider.value, 10) + num)).toFixed(2);
+    priceValue.textContent = (parseInt(slider.value) / 2).toFixed(2);
 }
 
