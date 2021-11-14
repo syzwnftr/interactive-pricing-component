b9f2ca5136a1931ccf8a6ee2ada4dead3b716687
Syazwan Fitri <syazfitry69@gmail.com>
2021-11-14 19:18:45 +0800

    Added function to change price and pageview when range slider is moved

--- script.js
+++ script.js
@@ -2,11 +2,59 @@ const slider = document.querySelector('.slider');
 const pageViewValue = document.querySelector('.pageviews-value');
 const priceValue = document.querySelector('.price-value');
 
-pageViewValue.textContent = slider.value;
-priceValue.textContent = (parseInt(slider.value) / 1.25).toFixed(2);
+
+if(slider.value === "0") {
+    pageViewValue.textContent = '10k';
+    priceValue.textContent = '8';
+}
+
+if(slider.value === "25") {
+    pageViewValue.textContent = '50k';
+    priceValue.textContent = '12';
+}
+
+if(slider.value === "50") {
+    pageViewValue.textContent = '100k';
+    priceValue.textContent = '16';
+}
+
+if(slider.value === "75") {
+    pageViewValue.textContent = '500k';
+    priceValue.textContent = '24';
+}
+
+if(slider.value === "100") {
+    pageViewValue.textContent = '1m';
+    priceValue.textContent = '32';
+}
+
+
+
 
 slider.oninput = function () {
-    pageViewValue.textContent = this.value;
-    priceValue.textContent = (parseInt(slider.value) / 1.25).toFixed(2);
+    if(slider.value === "0") {
+        pageViewValue.textContent = '10k';
+        priceValue.textContent = '8';
+    }
+    
+    if(slider.value === "25") {
+        pageViewValue.textContent = '50k';
+        priceValue.textContent = '12';
+    }
+    
+    if(slider.value === "50") {
+        pageViewValue.textContent = '100k';
+        priceValue.textContent = '16';
+    }
+    
+    if(slider.value === "75") {
+        pageViewValue.textContent = '500k';
+        priceValue.textContent = '24';
+    }
+    
+    if(slider.value === "100") {
+        pageViewValue.textContent = '1m';
+        priceValue.textContent = '32';
+    }
 }
 
