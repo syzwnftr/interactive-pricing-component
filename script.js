c7acefae776b1826165e499a657d7ed3007f03f2
Syazwan Fitri <syazfitry69@gmail.com>
2021-11-14 16:21:22 +0800

    Change price value

--- script.js
+++ script.js
@@ -2,14 +2,11 @@ const slider = document.querySelector('.slider');
 const pageViewValue = document.querySelector('.pageviews-value');
 const priceValue = document.querySelector('.price-value');
 
-
 pageViewValue.textContent = slider.value;
-priceValue.textContent = (parseInt(slider.value) / 2).toFixed(2);
-
-
+priceValue.textContent = (parseInt(slider.value) / 1.25).toFixed(2);
 
 slider.oninput = function () {
     pageViewValue.textContent = this.value;
-    priceValue.textContent = (parseInt(slider.value) / 2).toFixed(2);
+    priceValue.textContent = (parseInt(slider.value) / 1.25).toFixed(2);
 }
 
