874c2de6476bf5f4638d2bfbace1f72a54f4176b
Syazwan Fitri <syazfitry69@gmail.com>
2021-11-16 15:34:45 +0800

    Added new function

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const priceValue = document.querySelector('.price-value');
 const checkBox = document.querySelector('.check')
 let percent = document.querySelector('.percent');
 
+
 if(slider.value === "0") {
     pageViewValue.textContent = '10k';
     priceValue.textContent = '8.00';
@@ -29,150 +30,156 @@ if(slider.value === "100") {
     priceValue.textContent = '36.00';
 }
 
-
-slider.oninput = function () {
-    if(slider.value === "0") {
-        pageViewValue.textContent = '10k';
-        priceValue.textContent = '8.00';
-    }
-    
-    if(slider.value === "25") {
-        pageViewValue.textContent = '50k';
-        priceValue.textContent = '12.00';
-    }
-    
-    if(slider.value === "50") {
-        pageViewValue.textContent = '100k';
-        priceValue.textContent = '16.00';
-    }
-    
-    if(slider.value === "75") {
-        pageViewValue.textContent = '500k';
-        priceValue.textContent = '24.00';
-    }
-    
-    if(slider.value === "100") {
-        pageViewValue.textContent = '1m';
-        priceValue.textContent = '36.00';
-    }
+function progressColor() {
+    let slideValue = (slider.value / slider.max) * 100;
+    slider.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) ${slideValue}%,  hsl(224, 65%, 95%) ${slideValue}%)`;
 }
 
-function billing () {
 
-    if(checkBox.checked) {
-        percent.textContent = '-25%'
+// slider.oninput = function () {
 
-        if(slider.value === "0") {
-            pageViewValue.textContent = '10k';
-            priceValue.textContent = '6.00';
-        }
-        
-        if(slider.value === "25") {
-            pageViewValue.textContent = '50k';
-            priceValue.textContent = '9.00';
-        }
-        
-        if(slider.value === "50") {
-            pageViewValue.textContent = '100k';
-            priceValue.textContent = '12.00';
-        }
-        
-        if(slider.value === "75") {
-            pageViewValue.textContent = '500k';
-            priceValue.textContent = '18.00';
-        }
-        
-        if(slider.value === "100") {
-            pageViewValue.textContent = '1m';
-            priceValue.textContent = '27.00';
-        }
+//     if(slider.value === "0") {
+//         pageViewValue.textContent = '10k';
+//         priceValue.textContent = '8.00';
+//     }
+    
+//     if(slider.value === "25") {
+//         pageViewValue.textContent = '50k';
+//         priceValue.textContent = '12.00';
+//     }
+    
+//     if(slider.value === "50") {
+//         pageViewValue.textContent = '100k';
+//         priceValue.textContent = '16.00';
+//     }
+    
+//     if(slider.value === "75") {
+//         pageViewValue.textContent = '500k';
+//         priceValue.textContent = '24.00';
+//     }
+    
+//     if(slider.value === "100") {
+//         pageViewValue.textContent = '1m';
+//         priceValue.textContent = '36.00';
+//     }
+// }
 
-        slider.oninput = function () {
-            if(slider.value === "0") {
-                pageViewValue.textContent = '10k';
-                priceValue.textContent = '6.00';
-            }
-            
-            if(slider.value === "25") {
-                pageViewValue.textContent = '50k';
-                priceValue.textContent = '9.00';
-            }
-            
-            if(slider.value === "50") {
-                pageViewValue.textContent = '100k';
-                priceValue.textContent = '12.00';
-            }
-            
-            if(slider.value === "75") {
-                pageViewValue.textContent = '500k';
-                priceValue.textContent = '18.00';
-            }
-            
-            if(slider.value === "100") {
-                pageViewValue.textContent = '1m';
-                priceValue.textContent = '27.00';
-            }
-        }
+// function billing () {
 
-    } else {
-        percent.textContent = '-0%'
+//     if(checkBox.checked) {
+//         percent.textContent = '-25%'
 
-        if(slider.value === "0") {
-            pageViewValue.textContent = '10k';
-            priceValue.textContent = '8.00';
-        }
+//         if(slider.value === "0") {
+//             pageViewValue.textContent = '10k';
+//             priceValue.textContent = '6.00';
+//         }
         
-        if(slider.value === "25") {
-            pageViewValue.textContent = '50k';
-            priceValue.textContent = '12.00';
-        }
+//         if(slider.value === "25") {
+//             pageViewValue.textContent = '50k';
+//             priceValue.textContent = '9.00';
+//         }
         
-        if(slider.value === "50") {
-            pageViewValue.textContent = '100k';
-            priceValue.textContent = '16.00';
-        }
+//         if(slider.value === "50") {
+//             pageViewValue.textContent = '100k';
+//             priceValue.textContent = '12.00';
+//         }
         
-        if(slider.value === "75") {
-            pageViewValue.textContent = '500k';
-            priceValue.textContent = '24.00';
-        }
+//         if(slider.value === "75") {
+//             pageViewValue.textContent = '500k';
+//             priceValue.textContent = '18.00';
+//         }
         
-        if(slider.value === "100") {
-            pageViewValue.textContent = '1m';
-            priceValue.textContent = '36.00';
-        }
-        
-        
-        slider.oninput = function () {
-            if(slider.value === "0") {
-                pageViewValue.textContent = '10k';
-                priceValue.textContent = '8.00';
-            }
-            
-            if(slider.value === "25") {
-                pageViewValue.textContent = '50k';
-                priceValue.textContent = '12.00';
-            }
-            
-            if(slider.value === "50") {
-                pageViewValue.textContent = '100k';
-                priceValue.textContent = '16.00';
-            }
-            
-            if(slider.value === "75") {
-                pageViewValue.textContent = '500k';
-                priceValue.textContent = '24.00';
-            }
-            
-            if(slider.value === "100") {
-                pageViewValue.textContent = '1m';
-                priceValue.textContent = '36.00';
-            }
-        }
-    } 
-}
+//         if(slider.value === "100") {
+//             pageViewValue.textContent = '1m';
+//             priceValue.textContent = '27.00';
+//         }
 
-checkBox.addEventListener('click', billing);
+//         slider.oninput = function () {
+//             if(slider.value === "0") {
+//                 pageViewValue.textContent = '10k';
+//                 priceValue.textContent = '6.00';
+//             }
+            
+//             if(slider.value === "25") {
+//                 pageViewValue.textContent = '50k';
+//                 priceValue.textContent = '9.00';
+//             }
+            
+//             if(slider.value === "50") {
+//                 pageViewValue.textContent = '100k';
+//                 priceValue.textContent = '12.00';
+//             }
+            
+//             if(slider.value === "75") {
+//                 pageViewValue.textContent = '500k';
+//                 priceValue.textContent = '18.00';
+//             }
+            
+//             if(slider.value === "100") {
+//                 pageViewValue.textContent = '1m';
+//                 priceValue.textContent = '27.00';
+//             }
+//         }
+
+//     } else {
+//         percent.textContent = '-0%'
+
+
+//         if(slider.value === "0") {
+//             pageViewValue.textContent = '10k';
+//             priceValue.textContent = '8.00';
+//         }
+        
+//         if(slider.value === "25") {
+//             pageViewValue.textContent = '50k';
+//             priceValue.textContent = '12.00';
+//         }
+        
+//         if(slider.value === "50") {
+//             pageViewValue.textContent = '100k';
+//             priceValue.textContent = '16.00';
+//         }
+        
+//         if(slider.value === "75") {
+//             pageViewValue.textContent = '500k';
+//             priceValue.textContent = '24.00';
+//         }
+        
+//         if(slider.value === "100") {
+//             pageViewValue.textContent = '1m';
+//             priceValue.textContent = '36.00';
+//         }
+        
+//         slider.oninput = function () {
+//             if(slider.value === "0") {
+//                 pageViewValue.textContent = '10k';
+//                 priceValue.textContent = '8.00';
+//             }
+            
+//             if(slider.value === "25") {
+//                 pageViewValue.textContent = '50k';
+//                 priceValue.textContent = '12.00';
+//             }
+            
+//             if(slider.value === "50") {
+//                 pageViewValue.textContent = '100k';
+//                 priceValue.textContent = '16.00';
+//             }
+            
+//             if(slider.value === "75") {
+//                 pageViewValue.textContent = '500k';
+//                 priceValue.textContent = '24.00';
+//             }
+            
+//             if(slider.value === "100") {
+//                 pageViewValue.textContent = '1m';
+//                 priceValue.textContent = '36.00';
+//             }
+//         }
+//     } 
+// }
+
+// checkBox.addEventListener('click', billing);
 
 
 
