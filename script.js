c49fc4bfa6c609715c3abe4f2865ed76e79ea4d4
Syazwan Fitri <syazfitry69@gmail.com>
2021-11-17 08:54:55 +0800

    Fix slider not changing color when slide

--- script.js
+++ script.js
@@ -4,36 +4,40 @@ const priceValue = document.querySelector('.price-value');
 const checkBox = document.querySelector('.check')
 let percent = document.querySelector('.percent');
 
+let val = (slider.value / slider.max) * 100;
+slider.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) ${val}%, hsl(224, 65%, 95%) ${val}%)`;
 
-if(slider.value === "0") {
-    pageViewValue.textContent = '10k';
-    priceValue.textContent = '8.00';
+slider.oninput = function() {   
+    val = (slider.value / slider.max) * 100;
+    slider.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) ${val}%, hsl(224, 65%, 95%) ${val}%)`;
 }
 
-if(slider.value === "25") {
-    pageViewValue.textContent = '50k';
-    priceValue.textContent = '12.00';
-}
 
-if(slider.value === "50") {
-    pageViewValue.textContent = '100k';
-    priceValue.textContent = '16.00';
-}
+// if(slider.value === "0") {
+//     pageViewValue.textContent = '10k';
+//     priceValue.textContent = '8.00';
+// }
 
-if(slider.value === "75") {
-    pageViewValue.textContent = '500k';
-    priceValue.textContent = '24.00';
-}
+// if(slider.value === "25") {
+//     pageViewValue.textContent = '50k';
+//     priceValue.textContent = '12.00';
+// }
 
-if(slider.value === "100") {
-    pageViewValue.textContent = '1m';
-    priceValue.textContent = '36.00';
-}
+// if(slider.value === "50") {
+//     pageViewValue.textContent = '100k';
+//     priceValue.textContent = '16.00';
+// }
 
-function progressColor() {
-    let slideValue = (slider.value / slider.max) * 100;
-    slider.style.background = `linear-gradient(to right, hsl(174, 77%, 80%) ${slideValue}%,  hsl(224, 65%, 95%) ${slideValue}%)`;
-}
+// if(slider.value === "75") {
+//     pageViewValue.textContent = '500k';
+//     priceValue.textContent = '24.00';
+// }
+
+// if(slider.value === "100") {
+//     pageViewValue.textContent = '1m';
+//     priceValue.textContent = '36.00';
+// }
+
 
 
 // slider.oninput = function () {
